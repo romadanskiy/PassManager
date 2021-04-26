9f5cc3510f304825143241df8f38481f9f0d1366
Kirill <kirillaromad@mail.ru>
2021-04-27 00:45:45 +0300

    hosted db

--- PasswordManager/PasswordManager/Startup.cs
+++ PasswordManager/PasswordManager/Startup.cs
@@ -21,8 +21,10 @@ namespace PasswordManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string conStr = "Server=localhost;Port=5432;Database=PasswordManagerDB;User Id=postgres;Password=PAROLsekret777;";
-            services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(conStr));
+            const string localConStr = "Server=localhost;Port=5432;Database=PasswordManagerDB;User Id=postgres;Password=PAROLsekret777;";
+            const string herokuConStr = "Host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com;Database=d83gfranh6pqjq;Username=njudcwfmbpluwi;Password=afec3844e82fed075f576e9e261907c929604204d4036cda2f581e3e40d17c3a;sslmode=Require;TrustServerCertificate=true";
+            
+            services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(herokuConStr));
 
             services.AddIdentity<User, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationContext>()
