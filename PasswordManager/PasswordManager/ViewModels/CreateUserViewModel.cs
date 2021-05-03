fceb7572353a5ade1b6db655477dc5945ffb69f4
Kirill <kirillaromad@mail.ru>
2021-05-03 19:27:20 +0300

    vakidation fixes

--- PasswordManager/PasswordManager/ViewModels/CreateUserViewModel.cs
+++ PasswordManager/PasswordManager/ViewModels/CreateUserViewModel.cs
@@ -1,8 +1,14 @@
-﻿namespace PasswordManager.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.ViewModels
 {
     public class CreateUserViewModel
     {
+        [Display(Name = "Email")]
         public string Email { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
     }
 }
