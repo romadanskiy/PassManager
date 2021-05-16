9cb03cd94849004601fac0f21cb3db1e24c8e491
Kirill <kirillaromad@mail.ru>
2021-05-16 15:13:52 +0300

    added generator controller and view

---
+++ PasswordManager/PasswordManager/Controllers/GeneratorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordManager.ViewModels;
+
+namespace PasswordManager.Controllers
+{
+    public class GeneratorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Generate(string action, GeneratePasswordViewModel model)
+        {
+            switch (action)
+            {
+                case "generate":
+                    ViewBag.GeneratedPassword = "PaRol456!23";
+                    return View("Index");
+                case "save":
+                    return Content("Сохранить пароль");
+                default:
+                    return Content("Неопознанное действие");
+            }
+        }
+    }
+}
---
+++ PasswordManager/PasswordManager/ViewModels/GeneratePasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PasswordManager.ViewModels
+{
+    public class GeneratePasswordViewModel
+    {
+        public bool IsConfigured { get; set; }
+        public int PasswordLength { get; set; }
+        public bool HasLowercase { get; set; }
+        public bool HasUppercase { get; set; }
+        public bool HasDigit { get; set; }
+        public bool HasNonAlphanumeric { get; set; }
+    }
+}
