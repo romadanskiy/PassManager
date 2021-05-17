ada53f2629d9db5e1ad16723d7daccddf719ef59
Kirill <kirillaromad@mail.ru>
2021-05-17 15:55:14 +0300

    added form validation

--- PasswordManager/PasswordManager/wwwroot/js/generatorCheckboxes.js
+++ PasswordManager/PasswordManager/wwwroot/js/generatorCheckboxes.js
@@ -2,6 +2,7 @@
     let enableAllCheckBox = document.getElementById("enableAll");
     let enableAll = enableAllCheckBox.checked;
     let inputs = document.getElementsByClassName("check");
+    let checkBoxes = document.getElementsByClassName("check-box");
 
     if (!enableAll) {
         for (let i = 0; i < inputs.length; i++) {
@@ -22,4 +23,26 @@
             }
         }
     });
+    
+    let checkedCount = findChecked();
+    
+    function findChecked() {
+        let count = 0;
+        for (let i = 0; i < checkBoxes.length; i++) {
+            if (checkBoxes[i].checked)
+                count = count + 1;
+        }
+        return count;
+    }
+
+    for (let i = 0; i < checkBoxes.length; i++) {
+        checkBoxes[i].addEventListener('click', () => {
+            if (checkedCount === 1 && !checkBoxes[i].checked)
+                checkBoxes[i].checked = true;
+            else if (checkBoxes[i].checked)
+                checkedCount = checkedCount + 1;
+            else
+                checkedCount = checkedCount - 1;
+        });
+    }
 };
