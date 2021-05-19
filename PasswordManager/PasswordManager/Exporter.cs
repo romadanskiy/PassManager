141c7889b35dcfb79a2a0b8a25bc77544893cd03
Kirill <kirillaromad@mail.ru>
2021-05-19 14:15:45 +0300

    added credentials exporting

---
+++ PasswordManager/PasswordManager/Exporter.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PasswordManager
+{
+    public enum ExportType
+    {
+        Json,
+        Xml
+    }
+    
+    public class Exporter
+    {
+        public ExportType ExportType { get; set; }
+
+        public Exporter(ExportType exportType)
+        {
+            ExportType = exportType;
+        }
+
+        public string GenerateString<T>(T obj)
+        {
+            return ExportType switch
+            {
+                ExportType.Json => GenerateJsonString(obj),
+                ExportType.Xml => GenerateXmlString(obj),
+                _ => null
+            };
+        }
+
+        private string GenerateJsonString<T>(T obj)
+        {
+            var options = new JsonSerializerOptions {WriteIndented = true};
+            var result = JsonSerializer.Serialize(obj, options);
+            
+            return result;
+        }
+        
+        private string GenerateXmlString<T>(T obj)
+        {
+            var xmlSerializer = new XmlSerializer(obj.GetType());
+            using var stringWriter = new StringWriter();
+            using var xmlWriter = new XmlTextWriter(stringWriter) { Formatting = Formatting.Indented };
+            xmlSerializer.Serialize(xmlWriter, obj);
+            var xml = stringWriter.ToString();
+
+            return xml;
+        }
+    }
+}
---
+++ PasswordManager/PasswordManager/ViewModels/ExportImportCredentialViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace PasswordManager.ViewModels
+{
+    public class ExportImportCredentialViewModel
+    {
+        public string Source { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public ExportImportCredentialViewModel()
+        { }
+
+        public ExportImportCredentialViewModel(string source, string login, string password)
+        {
+            Source = source;
+            Login = login;
+            Password = password;
+        }
+    }
+}
