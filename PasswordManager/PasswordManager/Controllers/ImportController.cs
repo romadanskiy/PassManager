066cd81e3581f49b20f4f17b8532c2a7ae2e391b
Kirill <kirillaromad@mail.ru>
2021-05-20 00:29:17 +0300

    json importing

---
+++ PasswordManager/PasswordManager/Controllers/ImportController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PasswordManager.ViewModels;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace PasswordManager.Controllers
+{
+    public class ImportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Import(IFormFile upload)
+        {
+            if (upload == null) return RedirectToAction("Index");
+            
+            var contentType = upload.ContentType;
+
+            if (contentType != "application/json" && contentType != "application/xml")
+            {
+                TempData["importMessage"] = "Неподдерживаемый формат";
+                return RedirectToAction("Index");
+            }
+
+            var str = "";
+            using (var reader = new StreamReader(upload.OpenReadStream()))
+            {
+                str = reader.ReadToEnd();
+            }
+
+            return RedirectToAction("ImportResult", new {contentString = str, contentType = contentType});
+
+        }
+        
+        public IActionResult ImportResult(string contentString, string contentType)
+        {
+            var importer = contentType switch
+            {
+                "application/json" => new Importer(ImportType.Json),
+                "application/xml" => new Importer(ImportType.Xml),
+                _ => null
+            };
+            
+            var importResult = importer?.ParseContentString<List<ExportImportCredentialViewModel>>(contentString);
+            
+            return View(importResult);
+        }
+    }
+}
---
+++ PasswordManager/PasswordManager/Importer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using PasswordManager.ViewModels;
+
+namespace PasswordManager
+{
+    public enum ImportType
+    {
+        Json,
+        Xml
+    }
+    
+    public class Importer
+    {
+        public ImportType ImportType { get; set; }
+
+        public Importer(ImportType importType)
+        {
+            ImportType = importType;
+        }
+
+        public T ParseContentString<T>(string contentString)
+        {
+            return ImportType switch
+            {
+                ImportType.Json => ParseJsonContentString<T>(contentString),
+                ImportType.Xml => ParseXmlContentString<T>(contentString),
+                _ => default(T)
+            };
+        }
+
+        private T ParseJsonContentString<T>(string contentString)
+        {
+            var model = JsonSerializer.Deserialize<T>(contentString);
+            return model;
+        }
+
+        private T ParseXmlContentString<T>(string contentString)
+        {
+            return default(T);
+        }
+    }
+}
