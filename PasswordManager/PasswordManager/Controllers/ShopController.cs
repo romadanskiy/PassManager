3b525dae298732c32d8ab353ca343355b62a7145
Kirill <kirillaromad@mail.ru>
2021-05-23 13:53:11 +0300

    subscriptions buy action

--- PasswordManager/PasswordManager/Controllers/ShopController.cs
+++ PasswordManager/PasswordManager/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PasswordManager.Models;
@@ -8,6 +10,13 @@ namespace PasswordManager.Controllers
 {
     public class ShopController : Controller
     {
+        private UserManager<User> _userManager;
+        
+        public ShopController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        
         public IActionResult Subscriptions()
         {
             var context = new ApplicationContext();
@@ -22,6 +31,35 @@ namespace PasswordManager.Controllers
             var model = new SubscriptionsCatalogViewModel {AllSubscriptions = subscriptions, AllFeatures = features};
             
             return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BuySubscription(int subscriptionId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            
+            var context = new ApplicationContext();
+            var subscription = context.Subscriptions.FirstOrDefault(s => s.Id == subscriptionId);
+
+            if (subscription != null)
+            {
+                user.HasSubscription = true;
+                user.SubscriptionId = subscription.Id;
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Credentials");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+
+            return RedirectToAction("Subscriptions");
         }
     }
 }
