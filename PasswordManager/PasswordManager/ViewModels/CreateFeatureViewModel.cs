5c0f5fe21a96d1e18d4f94163edece8734f8e070
Kirill <kirillaromad@mail.ru>
2021-05-25 16:42:25 +0300

    features admin panel

---
+++ PasswordManager/PasswordManager/ViewModels/CreateFeatureViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.ViewModels
+{
+    public class CreateFeatureViewModel
+    {
+        [Required]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+        
+        [Required]
+        [Display(Name = "Название контроллера")]
+        public string ControllerName { get; set; }
+        
+        [Required]
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+    }
+}
---
+++ PasswordManager/PasswordManager/ViewModels/EditFeatureViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.ViewModels
+{
+    public class EditFeatureViewModel
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+        
+        [Required]
+        [Display(Name = "Название контроллера")]
+        public string ControllerName { get; set; }
+        
+        [Required]
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+    }
+}
