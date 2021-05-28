4a33ea004e038908aad6e881ebe090d44ffc9323
Kirill <kirillaromad@mail.ru>
2021-05-28 13:30:52 +0300

    installed plugin for table

---
+++ PasswordManager/PasswordManager/wwwroot/js/table.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+    $('#super-table').DataTable( {
+        "scrollX": true,
+        "language": {
+            url: '//cdn.datatables.net/plug-ins/1.10.24/i18n/Russian.json'
+        },
+        "columnDefs": [
+            {
+                "targets": [ 2 ],
+                "searchable": false,
+                "orderable": false
+            },
+            {
+                "targets": [ 3 ],
+                "searchable": false,
+                "orderable": false
+            }
+        ]
+    });
+} );
